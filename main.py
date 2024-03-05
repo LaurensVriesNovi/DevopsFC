42a82598d343b011802762e1b3b46d691be0990e
SamvdW <Sam.vd.Worp@novi-education.nlgit config --global user.email>
2024-03-05 14:50:15 +0100

    speler toegevoegd

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from pydantic import BaseModel,ConfigDict
+from typing import List
+app = FastAPI()
 
-App = FastAPI()
-
+spelers = [
+    {'naam_speler':  'Laurens', 'leeftijd': 24, 'afkomst': 'Nederland', 'statistieken': 55, 'transferwaarde': 20,
+     'naam_team': 'Ajax'},
+]
 
 class SpelerBase(BaseModel):
     naam_speler: str
@@ -12,16 +16,16 @@ class SpelerBase(BaseModel):
     transferwaarde: int
     naam_team: str
 
-
 class TeamBase(BaseModel):
     naam_team: str
     competitie: str
     totale_transferwaarde: int
-
 
 class competitieBase(BaseModel):
     naam_competitie: str
     land_competitie: str
 
 
-
+@app.get('/spelers/')
+async def get_spelers() -> list[SpelerBase]:
+    return spelers
