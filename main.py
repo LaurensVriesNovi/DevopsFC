8bcdcf050b6e0b4965e7c3256743571e97d854b1
SamvdW <Sam.vd.Worp@novi-education.nlgit config --global user.email>
2024-03-05 14:38:30 +0100

    Class aangemaakt

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel,ConfigDict
 
-DevOpsFCAPP = FastAPI()
+App = FastAPI()
+
+
+class SpelerBase(BaseModel):
+    naam: str
+    leeftijd: int
+    statistieken: int
+    transferwaarde: int
+    team_naam: str
+
+
+
