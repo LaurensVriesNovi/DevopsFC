bfdecee14b5ab9b3cfce3dd193341779ae11a955
prinl03 <lean.prins@novi-education.nl>
2024-03-28 12:58:08 +0100

    Alsjeblieft werk

--- database.py
+++ database.py
@@ -6,5 +6,5 @@ db_connection = psycopg2.connect(
     user=os.getenv("POSTGRES_USER"),
     password=os.getenv("POSTGRES_PASSWORD"),
     host=os.getenv("POSTGRES_HOST"),
-    port=os.getenv("POSTGRES_PORT")
+    port='25060'
 )
