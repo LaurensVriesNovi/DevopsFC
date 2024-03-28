5077ee3252ca92f6158af44f3f08a9713f94ccd0
prinl03 <lean.prins@novi-education.nl>
2024-03-28 12:35:59 +0100

    Test Docker

--- database.py
+++ database.py
@@ -2,9 +2,5 @@ import os
 import psycopg2
 
 db_connection = psycopg2.connect(
-    database=os.getenv("POSTGRES_DBNAME"),
-    user=os.getenv("POSTGRES_USER"),
-    password=os.getenv("POSTGRES_PASSWORD"),
-    host=os.getenv("POSTGRES_HOST"),
-    port=os.getenv("POSTGRES_PORT")
+    database=os.getenv("POSTGRES_URL"),
 )
