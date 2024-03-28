29cc88181f8abfe887895a5f3aee5a7201e98a4c
prinl03 <lean.prins@novi-education.nl>
2024-03-28 13:46:05 +0100

    Pipeline

--- database.py
+++ database.py
@@ -1,10 +1,4 @@
 import os
 import psycopg2
 
-db_connection = psycopg2.connect(
-    database=os.getenv("POSTGRES_DBNAME"),
-    user=os.getenv("POSTGRES_USER"),
-    password=os.getenv("POSTGRES_PASSWORD"),
-    host=os.getenv("POSTGRES_HOST"),
-    port='25060'
-)
+db_connection = psycopg2.connect("DATABASE_URL")
