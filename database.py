418a9c4955d5e86f62270cc1167b52c2557121a4
LaurensVriesNovi <161306642+LaurensVriesNovi@users.noreply.github.com>
2024-04-08 14:05:12 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,10 @@
 import os
 import psycopg2
 
-DATABASE_URL = os.environ.get('TEST_DATABASE_URL')
+DATABASE_URL = os.environ.get('DATABASE_URL')
+if os.environ.get('ENVIROMENT') == "TESTING"
+    DATABASE_URL = os.environ.get('TEST_DATABASE_URL')
+
 db_connection = psycopg2.connect(DATABASE_URL)
 
 def fetch_tuples(query):
