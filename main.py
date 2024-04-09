92b7233328184e74983123f7f081e5970ba9ee1d
prinl03 <lean.prins@novi-education.nl>
2024-04-09 14:17:04 +0200

    Logging

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ from competities import competities_router
 from teams import teams_router
 from spelers import spelers_router
 
-
+from logger import logger
+from middleware import log_middleware
+from starlette.middleware.base import BaseHTTPMiddleware, BaseHTTPMiddleware
 app = FastAPI()
+app.add_middleware(BaseHTTPMiddleware, dispatch=log_middleware)
+logger.info('Starting API.')
+
+
 app.include_router(spelers_router)
 app.include_router(teams_router)
 app.include_router(competities_router)
