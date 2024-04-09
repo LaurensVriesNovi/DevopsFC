bf6077827e5e54a538b6d036d189075d61349c30
LaurensVriesNovi <161306642+LaurensVriesNovi@users.noreply.github.com>
2024-04-09 10:37:54 +0200

    Update database.py

--- database.py
+++ database.py
@@ -3,6 +3,7 @@ import psycopg2
 
 if os.environ.get('PROD_ENVIRONMENT') == "PRODUCTION":
     db_connection = psycopg2.connect(os.environ.get('DATABASE_URL'))
+    
 if os.environ.get('TEST_ENVIRONMENT') == "TESTING":
     db_connection = psycopg2.connect(dbname=os.environ.get('TEST_DATABASE_NAME'), user=os.environ.get('DATABASE_USERNAME'), password=os.environ.get('DATABASE_PASSWORD'), host=os.environ.get('DATABASE_HOST'), port=os.environ.get('DATABASE_PORT'))
 
