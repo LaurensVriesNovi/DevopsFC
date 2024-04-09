1f9e16498b8ee903a7743219124a380946c4aac4
LaurensVriesNovi <161306642+LaurensVriesNovi@users.noreply.github.com>
2024-04-09 15:02:53 +0200

    autocommit toegevoegd

--- database.py
+++ database.py
@@ -8,6 +8,7 @@ if os.environ.get('PROD_ENVIRONMENT') == "PRODUCTION":
 if os.environ.get('TEST_ENVIRONMENT') == "TESTING":
     db_connection = psycopg2.connect(dbname=os.environ.get('TEST_DATABASE_NAME'), user=os.environ.get('DATABASE_USERNAME'), password=os.environ.get('DATABASE_PASSWORD'), host=os.environ.get('DATABASE_HOST'), port=os.environ.get('DATABASE_PORT'))
 
+db_connection.autocommit = True
 
 def fetch_tuples(query):
     cursor = db_connection.cursor()
