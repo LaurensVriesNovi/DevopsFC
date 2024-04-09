1f945c77d627ebfac7c64b97e570ae77ef80c5f4
prinl03 <lean.prins@novi-education.nl>
2024-04-09 15:40:03 +0200

    Logging af

--- logger.py
+++ logger.py
@@ -8,16 +8,16 @@ token = os.environ.get('LOGGING_TOKEN')
 logger = logging.getLogger()
 
 formatter = logging.Formatter(
-    fmt="%(asctime)s - %(levelname)s - %(message)s"
+    fmt="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
 )
 
 stream_handler = logging.StreamHandler(sys.stdout)
-file_handler = logging.FileHandler('app.log')
-beter_stack_handler = LogtailHandler(source_token=token)
+better_stack_handler = LogtailHandler(source_token=token)
 
 stream_handler.setFormatter(formatter)
-file_handler.setFormatter(formatter)
+better_stack_handler.setFormatter(formatter)
 
-logger.handlers = [stream_handler, file_handler, beter_stack_handler]
+logger.handlers = [stream_handler, better_stack_handler]
 
 logger.setLevel(logging.INFO)
--- middleware.py
+++ middleware.py
@@ -1,21 +1,26 @@
 from fastapi import Request
-from logger import logger
+from logger import logger  # Importeer je logger
 import time
+
 async def log_middleware(request: Request, call_next):
-    start = time.time()
+    start_time = time.time()
 
     response = await call_next(request)
 
-    process_time = time.time() - start
+    process_time = time.time() - start_time
+
     log_dict = {
         'url': request.url.path,
         'method': request.method,
         'process_time': process_time,
         'status_code': response.status_code
     }
-    if process_time > 1.0:
-        logger.warn('Duurt lang', extra=log_dict)
-    else:
-        logger.info(log_dict, extra=log_dict)
+
+    if 400 <= log_dict['status_code'] < 500:
+        logger.warning(log_dict['status_code'], extra=log_dict)
+    elif 500 <= log_dict['status_code'] < 600:
+        logger.error(log_dict['status_code'], extra=log_dict)
+    elif process_time > 1.0:
+        logger.warning('Duurt lang', extra=log_dict)
 
     return response
