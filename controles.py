6c615a7a58a1a49681f9b0bdff6edc154c47729e
LaurensVriesNovi <laurens.devries@novi-education.nl>
2024-04-10 13:27:53 +0200

    racing http i.p.v. pitting

--- controles.py
+++ controles.py
@@ -4,15 +4,15 @@ from database import db_connection
 
 def leeftijf_controle(spelerleeftijd):
     if spelerleeftijd < 16:
-        return HTTPException(status_code=400, detail="Speler is te jong (jonger dan 16 jaar)")
+        raise HTTPException(status_code=400, detail="Speler is te jong (jonger dan 16 jaar)")
     elif spelerleeftijd > 45:
-        return HTTPException(status_code=400, detail="Speler is te oud (ouder dan 45 jaar)")
+        raise HTTPException(status_code=400, detail="Speler is te oud (ouder dan 45 jaar)")
 
 def statestiek_controle(spelerstatistiek):
     if spelerstatistiek < 10:
-        return HTTPException(status_code=400, detail="Speler is te slecht (statestiek kleiner dan 10)")
+        raise HTTPException(status_code=400, detail="Speler is te slecht (statestiek kleiner dan 10)")
     elif spelerstatistiek > 99:
-        return HTTPException(status_code=400, detail="Speler is te goed (statestiek groter dan 99)")
+        raise HTTPException(status_code=400, detail="Speler is te goed (statestiek groter dan 99)")
 
 def team_id_controle(teamid):
     cursor = db_connection.cursor()
@@ -20,7 +20,7 @@ def team_id_controle(teamid):
     team = cursor.fetchone()
     if team is None:
         cursor.close()
-        return HTTPException(status_code=404, detail="Team niet gevonden")
+        raise HTTPException(status_code=404, detail="Team niet gevonden")
     cursor.close()
 
 def speler_id_controle(speler_id):
@@ -29,7 +29,7 @@ def speler_id_controle(speler_id):
     speler = cursor.fetchone()  # Haal de speler op
     if speler is None:  # Controleer of de speler niet bestaat
         cursor.close()
-        return HTTPException(status_code=404, detail="Speler niet gevonden")
+        raise HTTPException(status_code=404, detail="Speler niet gevonden")
     cursor.close()
 
 def competitie_id_controle(competitie_id):
@@ -38,5 +38,5 @@ def competitie_id_controle(competitie_id):
     competitie = cursor.fetchone()
     if competitie is None:
         cursor.close()
-        return HTTPException(status_code=404, detail="Competitie niet gevonden")
+        raise HTTPException(status_code=404, detail="Competitie niet gevonden")
     cursor.close()
